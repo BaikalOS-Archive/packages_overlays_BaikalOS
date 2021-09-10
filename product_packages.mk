AICP/packages_overlays_AICP

Introduce Brightness Slider Styles [1/2]
Change-Id: I65029df6630fe06f1d33b00f821dea97d4478256

product_packages.mk
@@ -5,3 +5,4 @@ include packages/overlays/AICP/product_packages_background_themes.mk
 include packages/overlays/AICP/product_packages_navbar_styles.mk
 include packages/overlays/AICP/product_packages_notif_styles.mk
 include packages/overlays/AICP/product_packages_qs_styles.mk
+include packages/overlays/AICP/product_packages_slider_styles.mk

BaikalOS-Archive/packages_overlays_BaikalOS

product_packages.mk
@@ -5,3 +5,4 @@ include packages/overlays/BaikalOS/product_packages_background_themes.mk
 include packages/overlays/BaikalOS/product_packages_navbar_styles.mk
 include packages/overlays/BaikalOS/product_packages_notif_styles.mk
 include packages/overlays/BaikalOS/product_packages_qs_styles.mk
+include packages/overlays/BaikalOS/product_packages_slider_styles.mk