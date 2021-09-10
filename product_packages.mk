AICP/packages_overlays_AICP

aicp_overlays: Introduce Immersive Navigation [3/3]
* IMPORTANT: added build product package in vendor

Based in Hide gesture navbar from Syberia project

Co-authored-by: DennySPb <dennyspb@gmail.com>
Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>

Change-Id: I339b239ed87ab3c493085164aaa5edd786e158bc

product_packages.mk
@@ -5,3 +5,4 @@ include packages/overlays/AICP/product_packages_background_themes.mk
 include packages/overlays/AICP/product_packages_navbar_styles.mk
 include packages/overlays/AICP/product_packages_notif_styles.mk
 include packages/overlays/AICP/product_packages_qs_styles.mk
+include packages/overlays/AICP/product_packages_nav_overlays.mk

BaikalOS-Archive/packages_overlays_BaikalOS

product_packages.mk
@@ -6,3 +6,4 @@ include packages/overlays/BaikalOS/product_packages_navbar_styles.mk
 include packages/overlays/BaikalOS/product_packages_notif_styles.mk
 include packages/overlays/BaikalOS/product_packages_qs_styles.mk
 include packages/overlays/BaikalOS/product_packages_slider_styles.mk
+include packages/overlays/BaikalOS/product_packages_nav_overlays.mk